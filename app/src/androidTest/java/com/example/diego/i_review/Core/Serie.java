9882addad64492d49f2baa1ba873f2db1dd1f76d
DiegoSelles <diegoselles10@gmail.com>
2016-11-23 12:52:07 +0100

    Add files via upload

---
+++ app/src/androidTest/java/com/example/diego/i_review/Core/Serie.java
@@ -0,0 +1,18 @@
+package com.example.diego.i_review.Core;
+
+/**
+ * Created by Diego on 23/11/2016.
+ */
+
+public class Serie {
+    private String nombre;
+
+    public Serie (String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+    
+}
---
+++ app/src/androidTest/java/com/example/diego/i_review/view/SeriesActivity.java
@@ -0,0 +1,15 @@
+package com.example.diego.i_review.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.diego.i_review.R;
+
+public class SeriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_series);
+    }
+}

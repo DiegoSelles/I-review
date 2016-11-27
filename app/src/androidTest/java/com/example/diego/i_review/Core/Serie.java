be1220f02c39f80ab01a497cc3a196e50950db2e
DiegoSelles <diegoselles10@gmail.com>
2016-11-27 23:51:21 +0100

    almacenamiento SQLite

--- app/src/androidTest/java/com/example/diego/i_review/Core/Serie.java
+++ app/src/androidTest/java/com/example/diego/i_review/Core/Serie.java
@@ -5,14 +5,34 @@ package com.example.diego.i_review.Core;
  */
 
 public class Serie {
+    private int id;
     private String nombre;
+    private int valoracion;
 
-    public Serie (String nombre){
+    public Serie ( String nombre){
+        this.id = id;
         this.nombre = nombre;
+        //this.valoracion = valoracion;
     }
 
     public String getNombre(){
         return nombre;
     }
-    
+    public int getId() { return id; }
+    public int getValoracion() {return valoracion;}
+
+    public String toString(){
+        return this.getNombre(); //+ ". Valoracion.:" + this.getValoracion();
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public void setId (int id){
+        this.id = id;
+    }
+    public void setValoracion(){
+        this.valoracion = valoracion;
+    }
 }
+
---
+++ app/src/androidTest/java/com/example/diego/i_review/Core/SqlIO.java
@@ -0,0 +1,50 @@
+package com.example.diego.i_review.Core;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Diego on 27/11/2016.
+ */
+
+public class SqlIO extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "ListaSeries";
+    private static final int DATABASE_VERSION = 2;
+
+    public SqlIO(Context context)
+    {
+        super( context, DATABASE_NAME, null, DATABASE_VERSION );
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.beginTransaction();
+        try{
+            db.execSQL( "CREATE TABLE IF NOT EXISTS serie( "
+                    + "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "
+                    + "nombre string(255), "
+                    + "valoracion int)");
+            db.setTransactionSuccessful();
+        }finally {
+            db.endTransaction();
+        }
+    }
+
+    public void onUpgrade(SQLiteDatabase db,int oldVersion, int newVersion){
+        Log.i(  "I-review.SqlIO",
+                "Actualizando BBDD de version " + oldVersion + " a la " + newVersion );
+
+        try {
+            db.beginTransaction();
+            db.execSQL( "DROP TABLE IF EXISTS serie");
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+        }
+
+        this.onCreate( db );
+
+    }
+}
